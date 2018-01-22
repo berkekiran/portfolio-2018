e89b654503c5a6710a03059de405f53d191323cd
berkekiran <berkekiranofficial@hotmail.com>
2018-01-22 07:30:22 +0300

    b update #1

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,43 +1,20 @@
 var app = angular.module("portfolio", []);
 
-app.controller('charactersController', function($scope) {
+app.controller('worksController', function($scope) {
 
   var vm = this;
-  vm.character = characters;
-  vm.limit = 2;
+  vm.work = works;
+  vm.limit = works.length;
 
   $scope.loadMore = function() {
-  var increamented = vm.limit + 2;
-  vm.limit = increamented > vm.character.length ? vm.character.length : increamented;
+  var increamented = vm.limit + works.length;
+  vm.limit = increamented > vm.work.length ? vm.work.length : increamented;
 
  };
 
 });
 
-app.controller('websitesController', function($scope) {
-
-   var vm = this;
-   vm.website = websites;
-   vm.limit = 2;
-
-   $scope.loadMore = function() {
-   var increamented = vm.limit + 2;
-   vm.limit = increamented > vm.website.length ? vm.website.length : increamented;
-
-  };
-
-});
-
-app.controller('gamesController', function($scope) {
-
-  var vm = this;
-  vm.game = games;
-  vm.limit = 2;
-
-  $scope.loadMore = function() {
-  var increamented = vm.limit + 2;
-  vm.limit = increamented > vm.game.length ? vm.game.length : increamented;
-
- };
-
+$( document ).ready(function() {
+    var numberofworks = ( works.length * 440 ) + 240 ;
+    $(".works").css({'width': numberofworks + 'px'});
 });
